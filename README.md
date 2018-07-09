164898a3085d4f14910b21615e9d46257250556b
Marques Johansson <marques@displague.com>
2018-07-09 14:57:10 -0400

    initial commit for docker-machine-linode against rancher2

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
-# ui-driver-skel
-Skeleton Rancher UI driver for custom docker-machine drivers
+# ui-driver-linode
 
-**Note: The Master branch works with Rancher 2.x+, if you are building a custom driver for Rancher 1.x use the 1.x branch**
+Linode Rancher UI driver for the Linode docker-machine driver
+
 
 ## Setup
 
-* Fork this repository into your own account as `ui-driver-DRIVERNAME`
-  * DRIVERNAME should be the name of the driver that you would give to `docker-machine create --driver`, e.g. "mycompany", "digitalocean", "vultr", etc.
-* Update the "name" in package.json to match
-  * You should also update description, URLs, etc, but these aren't strictly required.
-* `npm install`
+* Add a Machine Driver in Rancher 2.0 (Global -> Node Drivers)
+  * Name: Linode
+  * Download URL: `https://github.com/linode/docker-machine-driver-linode/releases/download/v0.0.12/docker-machine-driver-linode_linux-amd64.zip`)
+  * Custom UI URL: `https://github.com/displague/ui-driver-linode/releases/download/v0.1.0/component.js`)
+* Wait for the driver to become "Active"
+* Go to Clusters -> Add Cluster, your driver and custom UI should show up.
 
 ## Development
 
 This package contains a small web-server that will serve up the custom driver UI at `http://localhost:3000/component.js`.  You can run this while developing and point the Rancher settings there.
 * `npm start`
-* The driver name can be optionally overridden: `npm start -- --name=DRIVERNAME`
+* The driver name can be optionally overridden: `npm start -- --name=linode`
 * The compiled files are viewable at http://localhost:3000.
 * **Note:** The development server does not currently automatically restart when files are changed.
-* Do not use the `model.<drivername>Confg` signature to access your driver config in the template file, use the `config` alias that is already setup in the component
+* Do not use the `model.linodeConfg` signature to access your driver config in the template file, use the `config` alias that is already setup in the component
 
 ## Building
 
@@ -27,12 +28,3 @@ For other users to see your driver, you need to build it and host the output on 
 * `npm run build`
 * Copy the contents of the `dist` directory onto a webserver.
   * If your Rancher is configured to use HA or SSL, the server must also be available via HTTPS.
-
-## Using
-
-* Add a Machine Driver in Rancher 2.0 (Global -> Node Drivers)
-  * Name: Your `DRIVERNAME` (see above).
-  * Download URL: The URL for the driver binary (e.g. `https://github.com/mycompany/docker-machine-mycompany/releases/download/v1.0.0/docker-machine-driver-mycompany-v1.0.0-linux-amd64.tar.gz`)
-  * Custom UI URL: The URL you uploaded the `dist` folder to, e.g. `https://github.com/mycompany/ui-driver-mycompany/releases/download/v1.0.0/component.js`)
-* Wait for the driver to become "Active"
-* Go to Clusters -> Add Cluster, your driver and custom UI should show up.
--- component/component.js
+++ component/component.js
@@ -9,75 +9,108 @@ const LAYOUT;
 
 /*!!!!!!!!!!!GLOBAL CONST START!!!!!!!!!!!*/
 // EMBER API Access - if you need access to any of the Ember API's add them here in the same manner rather then import them via modules, since the dependencies exist in rancher we dont want to expor the modules in the amd def
-const computed     = Ember.computed;
-const get          = Ember.get;
-const set          = Ember.set;
-const alias        = Ember.computed.alias;
-const service      = Ember.inject.service;
+const computed = Ember.computed;
+const get = Ember.get;
+const set = Ember.set;
+const alias = Ember.computed.alias;
+const service = Ember.inject.service;
 
 const defaultRadix = 10;
-const defaultBase  = 1024;
+const defaultBase = 1024;
 /*!!!!!!!!!!!GLOBAL CONST END!!!!!!!!!!!*/
 
 
 
 /*!!!!!!!!!!!DO NOT CHANGE START!!!!!!!!!!!*/
 export default Ember.Component.extend(NodeDriver, {
-  driverName: '%%DRIVERNAME%%',
-  config:     alias('model.%%DRIVERNAME%%Config'),
-  app:        service(),
+driverName: '%%DRIVERNAME%%',
+needAPIToken: true,
+config: alias('model.%%DRIVERNAME%%Config'),
+app: service(),
 
-  init() {
-    // This does on the fly template compiling, if you mess with this :cry:
-    const decodedLayout = window.atob(LAYOUT);
-    const template      = Ember.HTMLBars.compile(decodedLayout, {
-      moduleName: 'nodes/components/driver-%%DRIVERNAME%%/template'
+init() {
+  // This does on the fly template compiling, if you mess with this :cry:
+  const decodedLayout = window.atob(LAYOUT);
+  const template = Ember.HTMLBars.compile(decodedLayout, {
+    moduleName: 'nodes/components/driver-%%DRIVERNAME%%/template'
+  });
+  set(this, 'layout', template);
+
+  this._super(...arguments);
+
+},
+/*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
+
+// Write your component here, starting with setting 'model' to a machine with your config populated
+bootstrap: function () {
+  // bootstrap is called by rancher ui on 'init', you're better off doing your setup here rather then the init function to ensure everything is setup correctly
+  let config = get(this, 'globalStore').createRecord({
+    type: '%%DRIVERNAME%%Config',
+    instanceType: 'g6-standard-4', // 4 GB Ram
+    region: 'us-east', // Newark
+    image: 'ubuntu18.04',
+    userData: ''
+  });
+
+  set(this, 'model.%%DRIVERNAME%%Config', config);
+},
+
+// Add custom validation beyond what can be done from the config API schema
+validate() {
+  // Get generic API validation errors
+  this._super();
+  var errors = get(this, 'errors') || [];
+  if (!get(this, 'model.name')) {
+    errors.push('Name is required');
+  }
+
+  // Add more specific errors
+
+  // Check something and add an error entry if it fails:
+  if (parseInt(get(this, 'config.memorySize'), defaultRadix) < defaultBase) {
+    errors.push('Memory Size must be at least 1024 MB');
+  }
+
+  // Set the array of errors for display,
+  // and return true if saving should continue.
+  if (get(errors, 'length')) {
+    set(this, 'errors', errors);
+    return false;
+  } else {
+    set(this, 'errors', null);
+    return true;
+  }
+},
+// Any computed properties or custom logic can go here
+actions: {
+  getData() {
+    this.set('gettingData', true);
+    let that = this;
+    Promise.all([this.apiRequest('/v4/regions'), this.apiRequest('/v4/images'), this.apiRequest('/v4/linode/types')]).then(function (responses) {
+      that.setProperties({
+        errors: [],
+        needAPIToken: false,
+        gettingData: false,
+        regionChoices: responses[0].data,
+        imageChoices: responses[1].data.filter(image => !/fedora/.test(image.label)),
+        sizeChoices: responses[2].data
+      });
+    }).catch(function (err) {
+      err.then(function (msg) {
+        that.setProperties({
+          errors: ['Error received from Linode: ' + msg.error.message],
+          gettingData: false
+        });
+      });
     });
-    set(this,'layout', template);
+  }
+},
 
-    this._super(...arguments);
-
-  },
-  /*!!!!!!!!!!!DO NOT CHANGE END!!!!!!!!!!!*/
-
-  // Write your component here, starting with setting 'model' to a machine with your config populated
-  bootstrap: function() {
-    // bootstrap is called by rancher ui on 'init', you're better off doing your setup here rather then the init function to ensure everything is setup correctly
-    let config = get(this, 'globalStore').createRecord({
-      type: '%%DRIVERNAME%%Config',
-      cpuCount: 2,
-      memorySize: 2048,
-    });
-
-    set(this, 'model.%%DRIVERNAME%%Config', config);
-  },
-
-  // Add custom validation beyond what can be done from the config API schema
-  validate() {
-    // Get generic API validation errors
-    this._super();
-    var errors = get(this, 'errors')||[];
-    if ( !get(this, 'model.name') ) {
-      errors.push('Name is required');
-    }
-
-    // Add more specific errors
-
-    // Check something and add an error entry if it fails:
-    if ( parseInt(get(this, 'config.memorySize'), defaultRadix) < defaultBase ) {
-      errors.push('Memory Size must be at least 1024 MB');
-    }
-
-    // Set the array of errors for display,
-    // and return true if saving should continue.
-    if ( get(errors, 'length') ) {
-      set(this, 'errors', errors);
-      return false;
-    } else {
-      set(this, 'errors', null);
-      return true;
-    }
-  },
-
-  // Any computed properties or custom logic can go here
+apiRequest: function (path) {
+  return fetch('https://api.linode.com' + path, {
+    headers: {
+      'Authorization': 'Bearer ' + this.get('model.%%DRIVERNAME%%Config.apiToken'),
+    },
+  }).then(res => res.ok ? res.json() : Promise.reject(res.json()));
+},
 });
