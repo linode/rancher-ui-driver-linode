fd6675f50ffec6caad2cb4055ef3437653267c0c
Neil MacDougall <neil_macdougall@hotmail.com>
2022-09-17 18:37:07 +0100

    Add Ubuntu 22.04 support

--- component/component.js
+++ component/component.js
@@ -124,7 +124,7 @@ export default Ember.Component.extend(NodeDriver, {
           restricted: responses.regions.restricted,
           regionChoices: responses.regions.data.map(region => { region.label = region.id.slice(0, 4).toUpperCase() + region.id.slice(4) + " (" + region.country.toUpperCase() + ")"; return region }).sort((a, b) => String.prototype.localeCompare(a, b)),
           // Filter on valid images and ignore Kubernetes images
-          imageChoices: responses.images.data.filter(image => /^linode.(ubuntu20.04|ubuntu18.04|ubuntu16.04|debian10|debian9)/.test(image.id) && !image.id.includes('kube')).sort((a, b) => a.id > b.id),
+          imageChoices: responses.images.data.filter(image => /^linode.(ubuntu22.04|ubuntu20.04|ubuntu18.04|ubuntu16.04|debian10|debian9)/.test(image.id) && !image.id.includes('kube')).sort((a, b) => a.id > b.id),
           sizeChoices: responses.sizes.data.map(size => { size.disk /= 1024; size.memory /= 1024; return size }),
         });
       }).catch((err) => {
